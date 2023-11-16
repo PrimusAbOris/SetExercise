724451e567da3c2fa398f3e7d64ff53ad403df98
PrimusAbOris <guy.ross.rit@gmail.com>
2023-11-16 00:02:37 -0500

    Update SetExerciseApplication.java
    Removed fork, further sketching

--- src/main/SetExerciseApplication.java
+++ src/main/SetExerciseApplication.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,14 +11,27 @@ public class SetExerciseApplication {
 	
 	Set<String> PokerSet = new HashSet<>();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		// extract the data from the CSV
 		BufferedReader br = new BufferedReader(new FileReader("PokerHands.csv"));
-		br.readLine();
+		
+		String currentLine = new String();
+		
+		while (br.readLine() != null) {
+			currentLine = br.readLine();
+			PokerSet.add(currentLine.split(",")[0]); //why is this asking for a static set??
+		}
+		br.close();
 		
 		
 		// remove ACE HIGH and QUEEN HIGH
+		for (String handType : PokerSet) {
+			if (handType == "ACE HIGH" || handType == "QUEEN HIGH") {
+				
+			}
+			
+		}
 		
 		
 		// Update PAIR to DEUCES
